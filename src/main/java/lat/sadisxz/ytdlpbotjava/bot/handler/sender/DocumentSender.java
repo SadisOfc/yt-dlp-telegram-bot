1d85b11461d986a4e885d551e984ccab338c4bb4
Sadi <rirfen5@gmail.com>
2025-12-31 18:50:39 -0500

    Implemented classes thah send multimedia content: - AudioSender - DocumentSender - VideoSender

---
+++ src/main/java/lat/sadisxz/ytdlpbotjava/bot/handler/sender/DocumentSender.java
@@ -0,0 +1,24 @@
+package lat.sadisxz.ytdlpbotjava.bot.handler.sender;
+
+import lat.sadisxz.ytdlpbotjava.bot.handler.commands.FormatExecutor;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
+
+import java.nio.file.Path;
+
+
+@Component
+public class DocumentSender {
+    private final FormatExecutor formatExecutor;
+
+    public DocumentSender(FormatExecutor formatExecutor) {
+        this.formatExecutor = formatExecutor;
+    }
+
+    public SendDocument sendDocument(Path path, Long chatId, String[] message){
+        SendDocument sendDocument= new SendDocument();
+        sendDocument.setChatId(chatId);
+        sendDocument.setDocument(formatExecutor.sendFormatToUser(path,chatId, message));
+        return sendDocument;
+    }
+}
