f4f0c83a2dd3db8431796944a48667a9b3779e9a
Sadi <rirfen5@gmail.com>
2025-12-31 18:40:06 -0500

    Basic project structure

---
+++ src/test/java/lat/sadisxz/ytdlpbotjava/YtDlpBotJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package lat.sadisxz.ytdlpbotjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtDlpBotJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
