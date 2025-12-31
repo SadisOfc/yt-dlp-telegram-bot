be7cabd5f775651bc6f71b3af5bba67d87f70ffe
Sadi <rirfen5@gmail.com>
2025-12-31 18:51:21 -0500

    Added file handlers: - FileCleaner: Used by yt-dlp command executors to locate the downloaded content - FileLocator: Removes residual files after sending the content

---
+++ src/main/java/lat/sadisxz/ytdlpbotjava/bot/handler/FileCleaner.java
@@ -0,0 +1,32 @@
+package lat.sadisxz.ytdlpbotjava.bot.handler;
+
+import lat.sadisxz.ytdlpbotjava.bot.model.MediaType;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+@Component
+public class FileCleaner {
+    public String delete(Path dir, MediaType mediaType){
+        if (!Files.exists(dir) || !Files.isDirectory(dir)) {
+            return String.format("Nada que eliminar: %s", dir); // nada que borrar
+        }
+
+        try (Stream<Path> files = Files.list(dir)) {
+            files.forEach(path -> {
+                try {
+                    Files.deleteIfExists(path); // borra archivo o subarchivo
+                } catch (IOException e) {
+                    e.printStackTrace(); // o loguea error
+                }
+            });
+            return String.format("Se eliminó el archivo %s del directorio: %s", mediaType, dir);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return String.format("No se pudo eliminar el archivo: %s", dir);
+        }
+    }
+}
