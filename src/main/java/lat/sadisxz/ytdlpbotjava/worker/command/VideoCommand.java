bf8d735e6a77f8d9ea515939bc0ef0f89732dc76
vita <rirfen5@gmail.com>
2026-02-27 16:11:49 -0500

    MP4 download format fixed Now supported by WhatsApp

--- src/main/java/lat/sadisxz/ytdlpbotjava/worker/command/VideoCommand.java
+++ src/main/java/lat/sadisxz/ytdlpbotjava/worker/command/VideoCommand.java
@@ -19,9 +19,13 @@ public class VideoCommand implements YtDlpCommand{
         List<String> commandList = new ArrayList<>();
         commandList.add("yt-dlp");
         commandList.add("-f");
-        commandList.add("bv*+ba/b");
+        commandList.add("bv*[vcodec^=avc1]+ba[acodec^=mp4a]/b[ext=mp4][vcodec^=avc1]");
         commandList.add("--merge-output-format");
         commandList.add("mp4");
+        commandList.add("--recode-video");
+        commandList.add("mp4");
+        commandList.add("--postprocessor-args");
+        commandList.add("ffmpeg:-c:v libx264 -profile:v high -level 4.0 -pix_fmt yuv420p -preset medium -crf 23 -movflags +faststart -c:a aac -b:a 128k -r 30");
         commandList.add("-o");
         commandList.add(downloaderProperties.base_path()+chatId+"/mp4/%(title)s.%(ext)s");
         commandList.add(content[1]);
